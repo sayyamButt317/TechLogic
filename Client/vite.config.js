ecc4149822d0f0b0c47377fe8ff4c6bef066ca63
sayyambutt <sayyambutt317@gmail.com>
2024-07-29 23:59:18 +0500

    updated production link

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 server:{
   proxy: {
     '/api/v1': {
-      target: 'http://localhost:8000',
+      target: 'https://techlogic-production.up.railway.app',
     },
   },
 },
