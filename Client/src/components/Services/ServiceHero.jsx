77f2959b94ec8989dee5767ce520786fdb3c35e3
sayyambutt <sayyambutt317@gmail.com>
2024-08-01 14:46:41 +0500

    update image rendring

--- Client/src/components/Services/ServiceHero.jsx
+++ Client/src/components/Services/ServiceHero.jsx
@@ -7,7 +7,7 @@ function ServicePage() {
 
   return (
       
-        <div className="relative w-full py-16 bg-cover bg-center h-screen" style={{ backgroundImage: `url(${globe})` }}>
+        <div className=" w-full py-16 bg-cover bg-center h-screen" style={{ backgroundImage: `url(${globe})` }}>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 text-white text-center justify-between">
             <motion.div
                 initial={{opacity: 0, y: 20}}
