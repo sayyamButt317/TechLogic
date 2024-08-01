a07a500eddae0e14ebb34fbcbdd786507b3ee0f8
sayyambutt <sayyambutt317@gmail.com>
2024-08-01 15:03:22 +0500

    update configuration for production

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'https://techlogic-production.up.railway.app',
-      },
-    },
-  },
   plugins: [react()],
+  define: {
+    'process.env': {}
+  }
 });
