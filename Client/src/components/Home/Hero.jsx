460c0bc4222a61c8454106c7d0131b2eae7f8cac
sayyambutt <sayyambutt317@gmail.com>
2024-08-06 02:19:52 +0500

    update home Ui

--- Client/src/components/Home/Hero.jsx
+++ Client/src/components/Home/Hero.jsx
@@ -2,19 +2,41 @@ import { motion } from "framer-motion";
 import Sidebar from "./sidebar";
 import { useNavigate } from "react-router-dom";
 import manwithlaptop from "../../assets/manwithlaptop.png";
-import SocialIcons from "../../Utils/socialicons"
+import SocialIcons from "../../Utils/socialicons";
+
 function Hero() {
   const navigate = useNavigate();
   return (
-    <div className="relative w-full min-h-[1050px] bg-black overflow-hidden">
+    <div className="relative w-full min-h-screen bg-black overflow-hidden">
       {/* Blurred Background Image */}
       <div
         className="absolute top-0 left-0 w-full h-full bg-cover bg-center filter blur-sm md:blur"
         style={{ backgroundImage: `url(${manwithlaptop})` }}
       />
-      {/* Company Title and Description on the Left */}
-      <div className="container mx-auto px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32">
-        <div className="absolute top-1/3 left-4 md:left-16 lg:left-24 xl:left-32 2xl:left-48 flex flex-col items-start justify-center h-1/2 text-white z-20">
+      {/* Container for content */}
+      <div className="container mx-auto px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32 relative z-20 h-full flex flex-col justify-between">
+        {/* Heading */}
+        <div className="pt-20 md:pt-40 text-center w-full text-white">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
+            className="font-serif text-4xl md:text-6xl lg:text-7xl font-extrabold mb-4"
+          >
+            TURN YOUR VISION INTO <br />
+            <span className="text-purple-500">REALITY</span>
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.6 }}
+            className="text-lg md:text-xl lg:text-2xl"
+          >
+            Let's Start Building Together
+          </motion.p>
+        </div>
+        {/* Company Title and Description */}
+        <div className="pb-20 md:pb-40 flex flex-col items-start justify-center text-white">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -45,29 +67,9 @@ function Hero() {
               >
                 Contact Us
               </motion.button>
-             <SocialIcons/>
+              <SocialIcons />
             </div>
           </motion.div>
-        </div>
-        {/* Heading */}
-        <div className="absolute top-1/4 text-center ml-4 md:ml-6 text-white z-20">
-          <motion.h1
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.4 }}
-            className="font-serif text-4xl md:text-6xl lg:text-7xl font-extrabold mb-4"
-          >
-            TURN YOUR VISION INTO <br />
-            <span className="text-purple-500">REALITY</span>
-          </motion.h1>
-          <motion.p
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.6 }}
-            className="text-lg md:text-xl lg:text-2xl"
-          >
-            Let's Start Building Together
-          </motion.p>
         </div>
       </div>
 
